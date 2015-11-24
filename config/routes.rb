6828872610e8d11cfa48ee731cdaa86b9afd7d30
ayesha98 <missayeshah@hotmail.com>
2015-11-24 10:54:05 +0000

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'posts/hello_world'
-  get 'posts/What_Id_like_to_Get_Out_of_Villiers_Park'
+  get 'posts/What_Id_Like_To_Get_Out_Of_Villiers_Park'
 
   get 'home/index'
 
