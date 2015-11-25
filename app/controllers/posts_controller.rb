7bcf469aa88632770e1e9f245fa31f1032ec669b
ayesha98 <missayeshah@hotmail.com>
2015-11-25 18:25:40 +0000

    Update posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   def What_Id_Like_To_Get_Out_Of_Villiers_Park
-  def Problem_Solving_and_Being_Lazy
+    def Problem_Solving_and_Being_Lazy
+    end
   end
 end
